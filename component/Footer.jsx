bba4f0f39cf8455db8fcb15c886ba777aed69729
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-07-07 15:01:16 +0300

    finished

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,12 +1,24 @@
-import { Box } from "@chakra-ui/react"
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
+  const [Year, setYear] = useState();
+  useEffect(() => {
+    const getYear = () => setYear(new Date().getFullYear());
+    getYear();
+  }, []);
   return (
-    <Box textAlign="center" p ="5" color ="gray.500" borderTop ="1px" borderColor ="gray.100">
-        2022 Jo Real State
-
+    <Box
+      textAlign="center"
+      p="5"
+      color="gray.500"
+      borderTop="3px"
+      borderColor="gray.100"
+      marginTop={12}
+    >
+      All rights Reserved | Copyright &copy; {Year}
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- component/Layout.jsx
+++ component/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estate</title>
+        <title>Real State</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
